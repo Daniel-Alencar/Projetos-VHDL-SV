d43f3fbde9b670cb65378732782dafc1988956ed
Daniel-Alencar <danielalencar746@gmail.com>
2025-08-10 10:39:20 -0300

    teste

--- Verilog/colorlight_sync_fifo/sync_fifo_tb.v
+++ Verilog/colorlight_sync_fifo/sync_fifo_tb.v
@@ -1,103 +1,104 @@
 module sync_fifo_tb #(parameter DATA_BITS=8)(
     input clk_in, n_rst, fifo_empty_in, fifo_full_in, uart_rx_valid_in, uart_tx_ready_in,
     input [DATA_BITS-1:0] uart_rx_data_in, fifo_rd_data_in,    
-    output reg fifo_wr_en, fifo_rd_en, uart_tx_en,
-    output reg [DATA_BITS-1:0] fifo_wr_data_out, uart_tx_data_out
+    output fifo_wr_en, fifo_rd_en, uart_tx_en,
+    output [DATA_BITS-1:0] fifo_wr_data_out, uart_tx_data_out
 );
 
-    // Estados
     localparam reg [1:0]
-    IDLE = 2'b00,
-    WRITE_RUN = 2'b01,
-    READ_RUN = 2'b10;
+    wr_idle = 2'b00,
+    write = 2'b01,
+    full = 2'b10;
+    localparam reg [1:0]
+    rd_idle = 2'b00,
+    read = 2'b01,
+    empty = 2'b10;
 
-    reg [1:0] state, next_state;
-    // contar de 0 até 1023
-    reg [9:0] count, next_count;
+    // Declaração dos sinais
+    reg [DATA_BITS-1:0] fifo_wr_data_reg;
+    reg [DATA_BITS-1:0] uart_tx_data_reg;
+    reg fifo_wr_en_reg, fifo_rd_en_reg, uart_tx_en_reg;
 
-    // Registradores de saída
-    reg [DATA_BITS-1:0] next_fifo_wr_data_out;
-    reg next_fifo_wr_en, next_fifo_rd_en, next_uart_tx_en;
-    reg [DATA_BITS-1:0] next_uart_tx_data_out;
-
-    // Máquina de estados sequencial
-    always @(posedge clk_in, negedge n_rst) begin
+    // Máquina de estados de escrita no buffer
+    always @(posedge clk_in, negedge n_rst) begin: fifo_write
+        reg [1:0] state;
         if(~n_rst) begin
-            state <= IDLE;
-            count <= 0;
-
-            fifo_wr_data_out <= 0;
-            uart_tx_data_out <= 0;
-            fifo_wr_en <= 0;
-            fifo_rd_en <= 0;
-            uart_tx_en <= 0;
+            state <= wr_idle;
+            fifo_wr_data_reg <= 0;
+            fifo_wr_en_reg <= 1'b0;
         end
         else begin
-            state <= next_state;
-            count <= next_count;
-
-            fifo_wr_data_out <= next_fifo_wr_data_out;
-            uart_tx_data_out <= next_uart_tx_data_out;
-            fifo_wr_en <= next_fifo_wr_en;
-            fifo_rd_en <= next_fifo_rd_en;
-            uart_tx_en <= next_uart_tx_en;
+            case(state)
+                wr_idle: begin
+                    fifo_wr_data_reg <= 0;
+                    if(~fifo_full_in && uart_rx_valid_in && uart_rx_data_in == "w") begin
+                        state <= write;
+                    end
+                end
+                write: begin
+                    fifo_wr_en_reg <= 1'b1;
+                    state <= full;
+                end
+                full: begin
+                    fifo_wr_en_reg <= 1'b0;
+                    if(fifo_full_in) begin
+                        state <= wr_idle;
+                    end
+                    else begin
+                        fifo_wr_data_reg <= fifo_wr_data_reg + 1;
+                        state <= write;
+                    end
+                end
+            endcase
         end
     end
 
-    always @(*)
-    begin
-        // Valores padrão para todos os registradores
-        next_state = state;
-        next_count = count;
-
-        next_fifo_wr_en = 0;
-        next_fifo_rd_en = 0;
-        next_uart_tx_en = 0;
-        next_fifo_wr_data_out = fifo_wr_data_out;
-        next_uart_tx_data_out = uart_tx_data_out;
-
-        case(state)
-            IDLE: begin
-                if(uart_rx_valid_in) begin
-                    if(uart_rx_data_in == "w") begin
-                        next_state = WRITE_RUN;
-                        next_count = 0;
-                    end
-                    else if(uart_rx_data_in == "r") begin
-                        next_state = READ_RUN;
-                        next_count = 0;
+    // Máquina de estados de leitura no buffer
+    always @(posedge clk_in, negedge n_rst) begin: fifo_read
+        reg [1:0] state;
+        if(~n_rst) begin
+            state <= rd_idle;
+            uart_tx_data_reg <= 0;
+            fifo_rd_en_reg <= 1'b0;
+            uart_tx_en_reg <= 1'b0;
+        end
+        else begin
+            case(state)
+                rd_idle: begin
+                    uart_tx_en_reg <= 1'b0;
+                    if(~fifo_empty_in && uart_rx_valid_in && uart_rx_data_in == "r") begin
+                        state <= read;
                     end
                 end
-            end
-            WRITE_RUN: begin
-                next_fifo_wr_en = 1;
-                next_fifo_wr_data_out = count;
 
-                if (fifo_full_in || count == 255) begin
-                    next_state = IDLE;
-                    next_fifo_wr_en = 0;
-                end
-                else begin
-                    next_count = count + 1;
-                end
-            end
-            READ_RUN: begin
-                if (~fifo_empty_in && uart_tx_ready_in) begin
-                    next_fifo_rd_en = 1;
-                    next_uart_tx_en = 1;
-                    next_uart_tx_data_out = fifo_rd_data_in;
-                end
-                else begin
-                    next_fifo_rd_en = 0;
-                    next_uart_tx_en = 0;
+                read: begin
+                    uart_tx_en_reg <= 1'b0;
+                    if(uart_tx_ready_in) begin
+                        fifo_rd_en_reg <= 1'b1;
+                        uart_tx_data_reg <= fifo_rd_data_in;
+                        state <= empty;
+                    end
                 end
 
-                if (fifo_empty_in) begin
-                    next_state = IDLE;
+                empty: begin
+                    fifo_rd_en_reg <= 1'b0;
+                    uart_tx_en_reg <= 1'b1;
+                    if(~uart_tx_ready_in) begin
+                        if(fifo_empty_in)
+                            state <= rd_idle;
+                        else
+                            state <= read;
+                    end
                 end
-            end
-
-        endcase
+            endcase
+        end
     end
+
+    // Direcionamento dos registradores para as saídas
+    assign fifo_wr_data_out = fifo_wr_data_reg;
+    assign uart_tx_data_out = uart_tx_data_reg;
+    assign fifo_wr_en = fifo_wr_en_reg;
+    assign fifo_rd_en = fifo_rd_en_reg;
+    assign uart_tx_en = uart_tx_en_reg;
 
 endmodule
---
+++ Verilog/colorlight_sync_fifo/sync_fifo_tb_test.v
@@ -0,0 +1,103 @@
+module sync_fifo_tb_test #(parameter DATA_BITS=8)(
+    input clk_in, n_rst, fifo_empty_in, fifo_full_in, uart_rx_valid_in, uart_tx_ready_in,
+    input [DATA_BITS-1:0] uart_rx_data_in, fifo_rd_data_in,    
+    output reg fifo_wr_en, fifo_rd_en, uart_tx_en,
+    output reg [DATA_BITS-1:0] fifo_wr_data_out, uart_tx_data_out
+);
+
+    // Estados
+    localparam reg [1:0]
+    IDLE = 2'b00,
+    WRITE_RUN = 2'b01,
+    READ_RUN = 2'b10;
+
+    reg [1:0] state, next_state;
+    // contar de 0 até 1023
+    reg [9:0] count, next_count;
+
+    // Registradores de saída
+    reg [DATA_BITS-1:0] next_fifo_wr_data_out;
+    reg next_fifo_wr_en, next_fifo_rd_en, next_uart_tx_en;
+    reg [DATA_BITS-1:0] next_uart_tx_data_out;
+
+    // Máquina de estados sequencial
+    always @(posedge clk_in, negedge n_rst) begin
+        if(~n_rst) begin
+            state <= IDLE;
+            count <= 0;
+
+            fifo_wr_data_out <= 0;
+            uart_tx_data_out <= 0;
+            fifo_wr_en <= 0;
+            fifo_rd_en <= 0;
+            uart_tx_en <= 0;
+        end
+        else begin
+            state <= next_state;
+            count <= next_count;
+
+            fifo_wr_data_out <= next_fifo_wr_data_out;
+            uart_tx_data_out <= next_uart_tx_data_out;
+            fifo_wr_en <= next_fifo_wr_en;
+            fifo_rd_en <= next_fifo_rd_en;
+            uart_tx_en <= next_uart_tx_en;
+        end
+    end
+
+    always @(*)
+    begin
+        // Valores padrão para todos os registradores
+        next_state = state;
+        next_count = count;
+
+        next_fifo_wr_en = 0;
+        next_fifo_rd_en = 0;
+        next_uart_tx_en = 0;
+        next_fifo_wr_data_out = fifo_wr_data_out;
+        next_uart_tx_data_out = uart_tx_data_out;
+
+        case(state)
+            IDLE: begin
+                if(uart_rx_valid_in) begin
+                    if(uart_rx_data_in == "w") begin
+                        next_state = WRITE_RUN;
+                        next_count = 0;
+                    end
+                    else if(uart_rx_data_in == "r") begin
+                        next_state = READ_RUN;
+                        next_count = 0;
+                    end
+                end
+            end
+            WRITE_RUN: begin
+                next_fifo_wr_en = 1;
+                next_fifo_wr_data_out = count;
+
+                if (fifo_full_in || count == 255) begin
+                    next_state = IDLE;
+                    next_fifo_wr_en = 0;
+                end
+                else begin
+                    next_count = count + 1;
+                end
+            end
+            READ_RUN: begin
+                if (~fifo_empty_in && uart_tx_ready_in) begin
+                    next_fifo_rd_en = 1;
+                    next_uart_tx_en = 1;
+                    next_uart_tx_data_out = fifo_rd_data_in;
+                end
+                else begin
+                    next_fifo_rd_en = 0;
+                    next_uart_tx_en = 0;
+                end
+
+                if (fifo_empty_in) begin
+                    next_state = IDLE;
+                end
+            end
+
+        endcase
+    end
+
+endmodule
